630f18e0c103f80eb8695a89992118bd67ad8a3f
Amy <wilford.amy@gmail.com>
2022-12-27 18:32:57 -0500

    added basic styling and tested responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
+// variable to hold API key value
 let key = config.MY_KEY;
 
 // Create script tag & link google api url with config key
 let script = document.createElement("script");
 script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`;
 
+// Establish global variables
 let map;
 let markers = [];
 let marker;
@@ -13,63 +15,46 @@ let response;
 let pollutionLat;
 let pollutionLng;
 
-let coordinates;
-let lat = 43.6532;
-let lng = -79.3832;
-// let coordinates = { lat: 43.6532, lng: -79.3832 };
-// Function to initiate static map - center point Toronto, ON
-
-// function getValues() {
-//   let receivedLat = document.getElementById("lat");
-//   console.log(receivedLat.value);
-//   let receivedLng = document.getElementById("lng");
-//   lat = parseInt(receivedLat.value);
-//   console.log(lat);
-//   lng = parseInt(receivedLng.value);
-//   console.log(lng);
-//   // coordinates = { lat: 43.6532, lng: -79.3832 };
-
-//   initMap();
-// }
+// Function to initiate map - center point begins
 function initMap() {
-  coordinates = { lat: lat, lng: lng };
+  coordinates = { lat: 43.6532, lng: -79.3832 };
 
   map = new google.maps.Map(document.getElementById("map"), {
     center: coordinates,
     zoom: 7,
     disableDefaultUI: true,
   });
-
+  // establish google maps Geocoder
   geocoder = new google.maps.Geocoder();
 
+  // Create geocoder search input and set attributes
   const inputText = document.createElement("input");
-
   inputText.type = "text";
   inputText.placeholder = "Search your location";
 
+  // Create geocoder search button and set attributes
   const submitButton = document.createElement("input");
-
   submitButton.type = "button";
   submitButton.value = "Geocode";
   submitButton.classList.add("btn", "btn-success");
+  // Create geocoder clear button
+  // const clearButton = document.createElement("input");
 
-  const clearButton = document.createElement("input");
-
-  clearButton.type = "button";
-  clearButton.value = "Clear";
-  clearButton.classList.add("button", "button-secondary");
-  response = document.createElement("pre");
-  response.id = "response";
-  response.innerText = "";
-  responseDiv = document.createElement("div");
-  responseDiv.id = "response-container";
-  responseDiv.appendChild(response);
+  // clearButton.type = "button";
+  // clearButton.value = "Clear";
+  // clearButton.classList.add("button", "button-secondary");
+  // response = document.createElement("pre");
+  // response.id = "response";
+  // response.innerText = "";
+  // responseDiv = document.createElement("div");
+  // responseDiv.id = "response-container";
+  // responseDiv.appendChild(response);
 
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
   // map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
-  map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+  // map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
   marker = new google.maps.Marker({
     map,
   });
@@ -79,17 +64,19 @@ function initMap() {
   submitButton.addEventListener("click", () =>
     geocode({ address: inputText.value })
   );
-  clearButton.addEventListener("click", () => {
-    clear();
-  });
-  clear();
+  // clearButton.addEventListener("click", () => {
+  //   clear();
+  // });
+  // clear();
 
+  // Event listener to show lat and lng of area on click
   map.addListener("click", (e) => {
     placeMarkerAndPanTo(e.latLng, map);
   });
 
+  // Info window will show coordaintes
   let infoWindow = new google.maps.InfoWindow({
-    content: "Click the map to get Lat/Lng!",
+    content: "Zoom in to pin the pollution location",
     position: coordinates,
   });
 
@@ -109,19 +96,16 @@ function initMap() {
           mapsMouseEvent.latLng.toJSON().lng
       )
     );
-    console.log(JSON.stringify(mapsMouseEvent.latLng.toJSON().lat));
-    console.log(JSON.stringify(mapsMouseEvent.latLng.toJSON().lng));
     pollutionLat = JSON.stringify(mapsMouseEvent.latLng.toJSON().lat);
     pollutionLng = JSON.stringify(mapsMouseEvent.latLng.toJSON().lng);
     infoWindow.open(map);
   });
 }
-
 function clear() {
   marker.setMap(null);
-  responseDiv.style.display = "none";
 }
 
+// Function to reset map per geolocation search
 function geocode(request) {
   clear();
   geocoder
@@ -132,8 +116,6 @@ function geocode(request) {
       map.setCenter(results[0].geometry.location);
       marker.setPosition(results[0].geometry.location);
       marker.setMap(map);
-      responseDiv.style.display = "block";
-      response.innerText = JSON.stringify(result, null, 2);
       return results;
     })
     .catch((e) => {
@@ -149,6 +131,7 @@ function placeMarkerAndPanTo(latLng, map) {
   map.panTo(latLng);
 }
 
+// Function to pull information from submit form and submit a pollution report
 function submitPollutionReport(event) {
   event.preventDefault();
   let inputLat = document.getElementById("lat").value;
@@ -157,11 +140,12 @@ function submitPollutionReport(event) {
   let email = document.getElementById("email").value;
   let pollutionConcern = document.getElementById("pollutionConcern").value;
 
+  // If lat and lng inputs are filled in use entered data - otherwise will be set to pinned location
   if (inputLat || inputLng) {
     pollutionLat = inputLat;
     pollutionLng = inputLng;
   }
-
+  // Set report in an object
   let report = {
     pollutionLat,
     pollutionLng,
@@ -169,12 +153,13 @@ function submitPollutionReport(event) {
     email,
     pollutionConcern,
   };
-
   console.log(report);
-  console.log("working");
 }
 
+// On submit report button click, run submitPollutionReport function
 let submitReport = document.getElementById("submit-report");
 submitReport.addEventListener("click", submitPollutionReport);
+
 window.initMap = initMap;
+// Add generated script tag to file
 document.head.appendChild(script);
