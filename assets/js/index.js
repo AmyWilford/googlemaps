e9ef99482092f6f7adf66164bec86f4e7e405c27
Amy <wilford.amy@gmail.com>
2022-12-27 15:29:55 -0500

    pulling lat and lng for any map click and adding flag

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,31 +4,27 @@ let map;
 let markers = [];
 let script = document.createElement("script");
 let coordinates;
-
+let lat = 43.6532;
+let lng = -79.3832;
 // let coordinates = { lat: 43.6532, lng: -79.3832 };
 script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`;
 // Function to initiate static map - center point Toronto, ON
 
-function pageChange() {
-  const home = document.getElementById("home");
-  home.style.display = "none";
-  const body = document.getElementById("body");
-  body.style.display = "block";
-}
 function getValues() {
   let receivedLat = document.getElementById("lat");
   console.log(receivedLat.value);
   let receivedLng = document.getElementById("lng");
-  let lat = parseInt(receivedLat.value);
+  lat = parseInt(receivedLat.value);
   console.log(lat);
-  let lng = parseInt(receivedLng.value);
+  lng = parseInt(receivedLng.value);
   console.log(lng);
-  // coordinates = { lat: lat, lng: lng };
-  coordinates = { lat: 43.6532, lng: -79.3832 };
+  // coordinates = { lat: 43.6532, lng: -79.3832 };
 
   initMap();
 }
 function initMap() {
+  coordinates = { lat: lat, lng: lng };
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: coordinates,
     zoom: 7,
@@ -37,6 +33,29 @@ function initMap() {
 
   map.addListener("click", (e) => {
     placeMarkerAndPanTo(e.latLng, map);
+  });
+
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: coordinates,
+  });
+
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON().lat+ " & "+mapsMouseEvent.latLng.toJSON().lng )
+    );
+    console.log(JSON.stringify(mapsMouseEvent.latLng.toJSON().lat))
+    console.log(JSON.stringify(mapsMouseEvent.latLng.toJSON().lng))
+
+    infoWindow.open(map);
   });
 }
 
@@ -47,6 +66,7 @@ function placeMarkerAndPanTo(latLng, map) {
   });
   map.panTo(latLng);
 }
+window.initMap = initMap;
 document.head.appendChild(script);
 
 // map options - including location and zoom lelvel
